ef24550bd858ba8d3d8050852338da45bc29ca9b
ndzib <brunoserkwi@gmail.com>
2022-02-04 09:18:21 +0100

    initial commit

---
+++ src/main/java/in0r8tiv/entypeit/EntypeitApplication.java
@@ -0,0 +1,13 @@
+package in0r8tiv.entypeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntypeitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntypeitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in0r8tiv/entypeit/EntypeitApplicationTests.java
@@ -0,0 +1,13 @@
+package in0r8tiv.entypeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntypeitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
